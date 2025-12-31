689c134357e02fd325f4d7398f4faa6a624da59d
Joseph Nicholas <99PercentPerfection@gmail.com>
2025-12-31 18:22:17 -0500

    Initial Base Code to use AWS Rekognition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const uuid = require('uuid');
+
 function App() {
+  const [image, setImage] = useState('');
+  const [uploadResultMessage, setUploadResultMessage] = useState('Upload image to authenticate.')
+  const [imgName, setImgName] = useState('placeholder.png')
+  const [isAuth, setIsAuth] = useState(false);
+
+  //take image from frontend and send it to the visitor s3 bucket, then authenticate
+  function sendImage(e) {
+    e.preventDefault();
+    setImgName(image.name);
+
+    const visitorImageName = uuid.v4();
+
+    fetch(`https://r192ll7u53.execute-api.us-east-1.amazonaws.com/dev/jnvisitor-facial-images/${visitorImageName}.jpg`, { 
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'image/jpg', 
+      },
+      body: image
+    }).then(async () => {
+      const response = await authenticate(visitorImageName);
+
+      if (response.Message === 'Success') {
+        setIsAuth(true);
+        setUploadResultMessage(`Hello ${response['firstName']} ${response['lastName']}. You are authenticated.`)
+      }else{
+        setIsAuth(false);
+        setUploadResultMessage('Authentication Failed')
+      }
+    }).catch(err => {
+      setIsAuth(false);
+      setUploadResultMessage('There is an error during the authentication process. Please try again.')
+      console.error(err); 
+    })
+  }
+
+  //check if the jpg has the same face content as the facial images in the employee s3 bucket
+  async function authenticate(visitorImageName) {
+    const requestUrl = 'https://r192ll7u53.execute-api.us-east-1.amazonaws.com/dev/employee?' + new URLSearchParams({
+      objectKey: `${visitorImageName}.jpg`
+    });
+    return await fetch(requestUrl, {
+      method: 'GET',
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+    .then((data) => {
+      return data;
+    }).catch(err => console.error(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AWS Facial Recognition System</h1>
+      <form onSubmit={sendImage}>
+        <input type='file' name='image' onChange={e => setImage(e.target.files[0])}/>
+        <button type='submit'>Authenticate</button>
+      </form>
+      <div className={isAuth ? 'success' : 'failure'}>{uploadResultMessage}</div>
+      <img src={ require(`./visitors/${imgName}`)} alt='VisitorImage' height={250} width={250}/>
     </div>
   );
 }
